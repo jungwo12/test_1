e141ea5494d6ba278dddd22b94a6b6f55c44b6eb
DESKTOP-32QC7EO\leeju <leejungw40@gmail.com>
2021-05-26 19:34:08 +0900

    람ㅇ놀함노어

--- main.c
+++ main.c
@@ -1,10 +1,16 @@
 #include<stdio.h>
-
-int main(){
+int main()
+{
+	char name[10]; 
 	int age;
+	
+	printf("당신의 이름을 입력해주세요 : ");
+	scanf("%s",name);
 	
 	printf("당신의 나이를 입력하세요 : ");
 	scanf("%d",&age);
 	
-	printf("우와 나이가 %d살 이군요",age);
+	printf("안녕하세요 %s 나이가 %d살 이군요",name,age);
+	
+	return 0;
 }
